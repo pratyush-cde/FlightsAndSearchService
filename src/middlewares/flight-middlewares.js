b32cf7c44f4e4c7ae7375cfd9fe998fbfed25118
Pratyush <pratyush.0929@gmail.com>
2026-08-28 16:44:26 +0530

    Added flight-create middleware

---
+++ src/middlewares/flight-middlewares.js
@@ -0,0 +1,23 @@
+const validateCreateFlight = (req, res, next) => {
+  if (
+    !req.body.flightNumber ||
+    !req.body.airplaneId ||
+    !req.body.departureAirportId ||
+    !req.body.arrivalAirportId ||
+    !req.body.departureTime ||
+    !req.body.arrivalTime ||
+    !req.body.price
+  ) {
+    return res.status(400).json({
+      data: {},
+      success: false,
+      message: "invalid request body for create flight",
+      err: "missing mandatory properties to create a flight",
+    });
+  }
+  next();
+};
+
+module.exports = {
+  validateCreateFlight,
+};
